5e0094800e339cfa27e2ea09212fe2d193559361
Eryc <eryc.matos@liberfly.com.br>
2021-11-17 15:03:04 -0300

    varios erros disponiveis

--- forca.py
+++ forca.py
@@ -5,22 +5,30 @@ def jogar():
 
     palavra_secreta = "Bacana"
     letras_acertadas = ["_", "_", "_", "_", "_" ,"_"]
+    letras_faltando = str(letras_acertadas.count("_"))
 
     enforcou = False
     acertou = False
+    tentativas = 0
 
     while(not enforcou and not acertou):
 
         chute= input("Diga uma letra: ")
         chute= chute.strip()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-               letras_acertadas[index]   = letra 
-            index = index + 1
-        
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra 
+                index += 1
+        else:
+            tentativas += 1
+
+        enforcou = tentativas == 6
+
         print(letras_acertadas)
+        print('Ainda estao faltando {} letras'.format(letras_faltando))
 
     print("Fim do Jogo!!")
 
